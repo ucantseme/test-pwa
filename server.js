92bea7420b66d896e995ab8f5fdd0474db373093
Darren <darren.lee@oakda.tw>
2019-05-10 17:30:32 +0800

    modify router

--- server.js
+++ server.js
@@ -4,16 +4,9 @@ const app = express()
 app.use(express.static(__dirname + '/'))
 
 app.get('/', (req, res) => {
-  console.log(__dirname)
   res.sendFile(__dirname + '/index.html')
-})
-app.get('/b', (req, res) => {
-  res.sendFile(__dirname + '/b.html')
-})
-app.get('/skeleton', (req, res) => {
-  res.sendFile(__dirname + '/skeleton.html')
 })
 
 app.listen('3033', () => {
-  console.log('server 3033')
+  console.log('server is listening at port 3033')
 })
